4c37f944a1cd57e468a73ee854b4f28478f5d009
Lisias T <me@lisias.net>
2018-02-04 21:58:27 -0200

    Stubbing Atari TOS and MSX-DOS support.

---
+++ src/main/java/de/waldheinz/fs/disk/msx/MsxDos.java
@@ -0,0 +1,14 @@
+package de.waldheinz.fs.disk.msx;
+
+import java.io.IOException;
+
+import de.waldheinz.fs.BlockDevice;
+import de.waldheinz.fs.FileSystem;
+
+public class MsxDos {
+
+    public static FileSystem read(final BlockDevice device, final boolean readOnly) throws IOException {
+    	throw new IOException("Not Implemented yet!");
+	}
+
+}
